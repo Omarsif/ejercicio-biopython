6447887ab596c4d5ba262ebe07a1416539d67682
Omarsif <omarbarsai@hotmail.com>
2020-10-14 16:16:45 -0700

    Actualización de script

--- ScriptPython.py
+++ ScriptPython.py
@@ -1,1 +1,26 @@
+from Bio import SeqIO 
+
+import os
+
+'''
+Archivo de prueba:
+archivo = "/home/omar/ejercicio-biopython/orchid.gbk"
+'''
+
 def summarize_contents(archivo):
+
+    nombreDeArchivo = os.path.basename(archivo)
+
+    record = next(SeqIO.parse(archivo, "genbank"))
+    print ("name:", nombreDeArchivo)
+    print ("path:", archivo)
+
+    records = list(SeqIO.parse(archivo, "genbank"))
+    print("num_records: %i records" % len(records))
+
+    for i, record in enumerate(SeqIO.parse(archivo, "genbank")):
+        print("-id:", record.id)
+        print("position:")
+        for seq_feature in record.features :
+            print('Start: %d, Stop: %d'%(int(seq_feature.location.start), int(seq_feature.location.end)))
+summarize_contents(archivo)
